ebcb12cdb2dfe5b037f0f33d867e886f8330caea
ericjbarton <erockbart@gmail.com>
2022-12-31 17:32:23 -0500

    now there's TWO triangles and a square.

--- game.rb
+++ game.rb
@@ -4,11 +4,18 @@ set width: 800, height: 600, title: "Where am I going with this"
 
 s = Square.new
 t = Triangle.new(
- # x1: 320, y1: 50,
-   # x2: 540, y2: 430,
-   # x3: 100, y3: 430,
-  )
+  x1: 320, y1: 50,
+  x2: 540, y2: 430,
+  x3: 100, y3: 430,
+)
 
-s.color = "blue"
+t2 = Triangle.new(
+  x1: 420, y1: 150,
+  x2: 640, y2: 530,
+  x3: 200, y3: 530,
+)
+
+s.color = ["blue", "green", "red", "yellow"]
 t.color = ["red", "green", "blue"]
+t2.color = ["blue", "red", "green"]
 show
