f777919b27bf257c99ae012140b9f6f303806e93
ericjbarton <erockbart@gmail.com>
2023-01-02 10:33:21 -0500

    draws a bunch of boxes and they move

--- game2.rb
+++ game2.rb
@@ -1,7 +1,36 @@
-require 'ruby2d'
+require "ruby2d"
 
-set background: 'navy'
+set background: "navy"
 set width: 1920
 set height: 1080
 
-class 
+class Box
+  def initialize
+    @x = rand(Window.width)
+    @y = rand(Window.height)
+    @x_velocity = (-5..5).to_a.sample
+    @y_velocity = (-5..5).to_a.sample
+    @color = Color.new("random")
+  end
+
+  def draw
+    Square.new(x: @x, y: @y, size: 15, color: @color)
+  end
+
+  def move
+    @x = @x + @x_velocity
+    @y = @y + @y_velocity
+  end
+end
+
+$boxes = Array.new(40) { Box.new }
+
+update do
+  clear
+  $boxes.each do |box|
+    box.draw
+    box.move
+  end
+end
+
+show
